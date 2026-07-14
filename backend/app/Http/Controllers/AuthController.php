42393197d60d1d75e5f471881835ed896fb2b5dc
Hicham-Mek <mekidichehichem@gmail.com>
2026-07-14 17:01:39 +0100

    Implement authentication API with register and login

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\Auth\RegisterRequest;
 use App\Models\User;
+use App\Http\Requests\Auth\LoginRequest;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -20,5 +22,34 @@ class AuthController extends Controller
         'message' => 'User registered successfully.',
         'user' => $user,
     ], 201);
+}
+public function login(LoginRequest $request)
+{
+    if (! Auth::attempt($request->only('email', 'password'))) {
+        return response()->json([
+            'message' => 'Invalid credentials.',
+        ], 401);
+    }
+
+    // $request->session()->regenerate();
+
+    return response()->json([
+        'message' => 'Login successful.',
+        'user' => Auth::user(),
+    ], 200);
+}
+public function logout()
+{
+    Auth::logout();
+
+    return response()->json([
+        'message' => 'Logout successful.',
+    ], 200);
+}
+public function user()
+{
+    return response()->json([
+        'user' => Auth::user(),
+    ]);
 }
 }
