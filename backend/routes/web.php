9e3d267b99a59f82b40504c2ddc15dcd0e239ece
Hicham-Mek <mekidichehichem@gmail.com>
2026-08-02 18:04:39 +0100

    modify the env

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,9 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'message' => 'API is running',
+        'status' => 200
+    ]);
 });
